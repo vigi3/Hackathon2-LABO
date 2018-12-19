a12f0e1ec6b460dceadc08b53a257754f79c2e5e
younes youssefi <assoufiy@hotmail.fr>
2018-12-19 21:18:55 +0100

    make_crud_for_entity

---
+++ src/Form/CompanyType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('companyName')
+            ->add('managerName')
+            ->add('companyType')
+            ->add('mail')
+            ->add('comment')
+            ->add('events')
+            ->add('services')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
