24f40450bed4042858bedc091fce3f9d1e7d10dd
Jovanela Daribo <carly972@live.fr>
2018-12-20 22:35:43 +0100

    Reroutage du Crud

--- src/Controller/EventAdminController.php
+++ src/Controller/EventAdminController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/event/admin")
+ * @Route("/event")
  */
 class EventAdminController extends AbstractController
 {
