cd6cc49892b30674593b2deabdb7e03cf71ef5db
Jovanela Daribo <carly972@live.fr>
2018-12-21 11:54:54 +0100

    ...

---
+++ src/Controller/CategoryServiceController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Controller;
+use App\Entity\CategoryService;
+use App\Form\CategoryServiceType;
+use App\Repository\CategoryServiceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/category/service")
+ */
+class CategoryServiceController extends AbstractController
+{
+    /**
+     * @Route("/", name="category_service_index", methods={"GET"})
+     */
+    public function index(CategoryServiceRepository $categoryServiceRepository): Response
+    {
+        return $this->render('category_service/index.html.twig', ['category_services' => $categoryServiceRepository->findAll()]);
+    }
+    /**
+     * @Route("/new", name="category_service_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $categoryService = new CategoryService();
+        $form = $this->createForm(CategoryServiceType::class, $categoryService);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($categoryService);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_service_index');
+        }
+        return $this->render('category_service/new.html.twig', [
+            'category_service' => $categoryService,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="category_service_show", methods={"GET"})
+     */
+    public function show(CategoryService $categoryService): Response
+    {
+        return $this->render('category_service/show.html.twig', ['category_service' => $categoryService]);
+    }
+    /**
+     * @Route("/{id}/edit", name="category_service_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, CategoryService $categoryService): Response
+    {
+        $form = $this->createForm(CategoryServiceType::class, $categoryService);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('category_service_index', ['id' => $categoryService->getId()]);
+        }
+        return $this->render('category_service/edit.html.twig', [
+            'category_service' => $categoryService,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="category_service_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, CategoryService $categoryService): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$categoryService->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($categoryService);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('category_service_index');
+    }
+}
